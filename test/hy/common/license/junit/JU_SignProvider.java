3cce86381c24e3c5be39b4ca87a3dc0fdfe2db8b
ZhengWei <ZhengWei@ZhengWei_HY>
2017-07-25 22:19:28 +0800

    1.0.0
    Signed-off-by: ZhengWei <ZhengWei@ZhengWei_HY>

--- test/hy/common/license/junit/JU_SignProvider.java
+++ test/hy/common/license/junit/JU_SignProvider.java
@@ -25,12 +25,12 @@ public class JU_SignProvider
     @Test
     public void test_SignProvider()
     {
-        KeyStore v_KeyStore  = KeyStore.generater();
+        KeyStore v_KeyStore  = KeyStore.generater("A");
         String   v_PlainText = "ABCDEFG";
         byte []  v_Sign      = Signaturer.sign(v_KeyStore.getPrivateKey() ,v_PlainText);
         
-        System.out.println("-- 公匙：" + v_KeyStore.getPublicKey() .toString());
-        System.out.println("-- 私匙：" + v_KeyStore.getPrivateKey().toString());
+        System.out.println("-- 公匙：" + v_KeyStore.getPublicKeyString());
+        System.out.println("-- 私匙：" + v_KeyStore.getPrivateKeyString());
         System.out.println("-- 签名：" + new String(v_Sign));
         
         boolean v_Verify = SignProvider.verify(v_KeyStore.getPublicKey() ,v_PlainText ,v_Sign);
