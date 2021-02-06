e7a85587b8e81d166bb0c2f1a6241b13098ebe06
HY-ZhengWei <hy.zhengwei@qq.com>
2021-02-06 17:29:50 +0800

    1.1.1

--- test/hy/common/license/junit/JU_SHA256.java
+++ test/hy/common/license/junit/JU_SHA256.java
@@ -22,11 +22,13 @@ public class JU_SHA256
     {
         Hash v_Hash_SHA256_1 = new Hash(2 ,2 ,null ,true);
         Hash v_Hash_SHA256_2 = new Hash(2 ,2 ,null ,false);
-        Hash v_Hash_SHA512_2 = new Hash(2 ,3 ,null ,false);
+        Hash v_Hash_SHA256_3 = new Hash(2 ,3 ,null ,false);
+        Hash v_Hash_SHA512_1 = new Hash(2 ,5 ,null ,false);
         
         System.out.println(v_Hash_SHA256_1.encrypt("Hello World!"));
         System.out.println(v_Hash_SHA256_2.encrypt("Hello World!"));
-        System.out.println(v_Hash_SHA512_2.encrypt("Hello World!"));
+        System.out.println(v_Hash_SHA256_3.encrypt("Hello World!"));
+        System.out.println(v_Hash_SHA512_1.encrypt("Hello World!"));
     }
     
     
@@ -34,7 +36,7 @@ public class JU_SHA256
     @Test
     public void test_SMS()
     {
-        Hash v_Hash_SHA256_1 = new Hash(2 ,2 ,null ,false);
+        Hash v_Hash_SHA256_1 = new Hash(2 ,3 ,null ,false);
         
         System.out.println(v_Hash_SHA256_1.encrypt("AccountId=yanfa001&PhoneNos=13699999999&Password=B54B89712EB997BE99114478E3673E3F&Random=6203922&Timestamp=1532928860"));
     }
